d9ea48f465850b68d4cd156cf06584639ae936c4
Kikutada <Kikutada@me.com>
2022-10-03 00:41:11 +0900

    motion controls

--- 0008_GamePacman/GameScene.swift
+++ 0008_GamePacman/GameScene.swift
@@ -1,88 +1,94 @@
 //
 //  GameScene.swift
-//  0008_GamePacman
+//  0006_GamePlayTest
 //
-//  Created by 吉田己久忠 on 2020/09/29.
+//  Created by Kikutada on 2020/08/11.
+//  Copyright © 2020 Kikutada All rights reserved.
 //
 
 import SpriteKit
-import GameplayKit
+import CoreMotion
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    /// Main object with main game sequence
+    private var gameMain: CgGameMain!
+    
+    /// Points for Swipe operation
+    private var startPoint: CGPoint = CGPoint.init()
+    private var endPoint: CGPoint = CGPoint.init()
+
+    // MotionManager for accel
+    private var motionManager: CMMotionManager!
     
     override func didMove(to view: SKView) {
+
+        //  Create and start game sequence.
+        gameMain  = CgGameMain(skscene: self)
+        gameMain.startSequence()
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+        // Create motion manager
+        motionManager = CMMotionManager()
+        motionManager.accelerometerUpdateInterval = 0.05
+
+        motionManager.startAccelerometerUpdates(
+            to: OperationQueue.current!, withHandler: {
+                (accelData: CMAccelerometerData?, errorOC: Error?) in self.sendAccelEvent(acceleration: accelData!.acceleration)
+            }
+        )
+    }
+
+    // moton event
+    func sendAccelEvent(acceleration: CMAcceleration){
+        let x_diff: Int = Int(acceleration.x * 100)
+        let y_diff: Int = Int(acceleration.y * 100)
+
+        if abs(x_diff) > abs(y_diff) {
+            gameMain.sendEvent(message: .Accel, parameter: [Int(x_diff > 0 ? EnDirection.Right.rawValue : EnDirection.Left.rawValue)])
+        } else {
+            gameMain.sendEvent(message: .Accel, parameter: [Int(y_diff > 0 ? EnDirection.Up.rawValue : EnDirection.Down.rawValue)])
         }
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        // Get start touchpoint for swipe.
+        if let t = touches.first {
+            let location = t.location(in: self)
+            startPoint = CGPoint(x: location.x, y: location.y)
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        gameMain.sendEvent(message: .Touch, parameter: [Int(startPoint.x), Int(startPoint.y)])
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        // Get end touchpoint for swipe.
+        if let t = touches.first {
+            let location = t.location(in: self)
+            endPoint = CGPoint(x: location.x, y: location.y)
+            
+            let x_diff = endPoint.x - startPoint.x
+            let y_diff = endPoint.y - startPoint.y
+
+            // Send swipe message to GameMain.
+            if abs(x_diff) > abs(y_diff) {
+                gameMain.sendEvent(message: .Swipe, parameter: [Int(x_diff > 0 ? EnDirection.Right.rawValue : EnDirection.Left.rawValue)])
+            } else {
+                gameMain.sendEvent(message: .Swipe, parameter: [Int(y_diff > 0 ? EnDirection.Up.rawValue : EnDirection.Down.rawValue)])
+            }
+
+        }
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
-    
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        // Called before each frame is rendered.
+
+        // Send update message every 16ms.
+        gameMain.sendEvent(message: .Update, parameter: [SYSTEM_FRAME_TIME])
     }
+
 }
